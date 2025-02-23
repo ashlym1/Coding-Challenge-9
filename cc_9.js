2d06b4608029cf75c43291aca5c6222575c288d4
ashlym1 <ashlym3@usf.edu>
2025-02-23 16:50:24 -0500

    Task 3 - Created Company Class

--- cc_9.js
+++ cc_9.js
@@ -1,21 +1,20 @@
 // Task 1 Creating an Employee Class 
-
-    // Creating a HR system with emplyee class 
-    class Employee { 
-        constructor (name,id, department,salary){
-            this.name= name;  /// Employee's name 
-            this.id= id; // Employes Id number
-            this.department= department// their department 
-            this.salary= salary
+ // Creating a HR system with emplyee class 
+  class Employee { 
+ constructor (name,id, department,salary){
+  this.name= name;  /// Employee's name 
+  this.id= id; // Employes Id number
+  this.department= department// their department 
+  this.salary= salary
         };
-        // Method that returns employee details :
-        getDetails () { 
-            return `Emplyee:${this.name}, ID: ${this.ID},
-            Department: ${this.department}, Salary:$${this.salary}`;
+    // Method that returns employee details :
+     getDetails () { 
+     return `Emplyee:${this.name}, ID: ${this.ID},
+     Department: ${this.department}, Salary:$${this.salary}`;
         }
         // Method that return the employyes annual salary : 
     calculateAnnualSalary() {
-        return this.salary*12;
+     return this.salary*12;
     }
     };//
     // Test Case: 
@@ -29,9 +28,9 @@
     // Task 2- Created Manager Class with inhereitance 
       // Class manager inherits from employee : 
             class Manager extends Employee {
-                constructor(name, id, department, salary, teamSize) {
-                        super(name, id, department, salary); // calling employee constructor 
-                     this.teamSize = teamSize; // adding a new property 
+             constructor(name, id, department, salary, teamSize) {
+            super(name, id, department, salary); // calling employee constructor 
+            this.teamSize = teamSize; // adding a new property 
         }
          // Overriding getDetails() to include the team size
       getDetails() {
@@ -51,3 +50,25 @@
     
     console.log(mgr1.calculateBonus()); 
     // Expected output: 9600 
+
+    // Task 3- Creating a Company class 
+    class comapny { 
+        constructor (name) { 
+            this.name=name;// the companys name 
+            this.employees= [] ; // created an array to store the employee objects 
+        }
+        //Method to add an employee to the company
+        addEmployee(employee) {
+            this.employees.forEach ((employee)=> {
+                console.log(employee.getDetails());
+            });
+            }
+        }
+    // Test Cases : 
+    const company = new Company("TechCorp");
+company.addEmployee(emp1);
+company.addEmployee(mgr1);
+company.listEmployees();
+// Expected output:
+// "Employee: Alice Johnson, ID: 101, Department: Sales, Salary: $5000"
+// "Manager: John Smith, ID: 201, Department: IT, Salary: $8000, Team Size: 5"
